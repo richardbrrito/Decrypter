929a817e6366b608ab1029ae5ca1e72771581afd
Richard Brito <117929922+richardbrrito@users.noreply.github.com>
2023-09-28 12:45:58 -0400

    Update Transpose.java
    completed decode method

--- src/Transpose.java
+++ src/Transpose.java
@@ -1,17 +1,16 @@
-public class Transpose extends Cipher
-{
-    Transpose(String s)
-    {
+public class Transpose extends Cipher {
+
+    Transpose(String s) {
         super(s);
     }
-    public String encode(String word)
-    {
+
+    public String encode(String word) {
         StringBuffer result = new StringBuffer(word);
         result.reverse();
         return result.toString();
     }
-    public String decode(String word)
-    {
-// Complete this method so that it reverses the encoded string;
+
+    public String decode(String word) {
+        return new StringBuffer(word).reverse().toString();
     }
 }
