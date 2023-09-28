00a8c24965ce237f54d1acfced7a1db5f96a49d9
Richard Brito <117929922+richardbrrito@users.noreply.github.com>
2023-09-28 12:43:55 -0400

    Update Cipher.java
    updated cipher class and added changes said in comments

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,42 +1,43 @@
 import java.util.StringTokenizer;
-public abstract class Cipher
-{
+
+public abstract class Cipher {
     private String message;
     StringBuffer encrypted_message, decrypted_message;
-    public Cipher(String text)
-    {
-// Complete the constructor definition
+
+    public Cipher(String text) {
+        this.message = text;
+        decrypted_message = new StringBuffer();
     }
-    public final void encrypt()
-    {
-        /* The message string is tokenized into individual words,
-         * and each word is encoded by calling the encode method
-         */
+
+    public final void encrypt() {
         encrypted_message = new StringBuffer();
         StringTokenizer words = new StringTokenizer(message);
-        while(words.hasMoreTokens())
-        {
+        while (words.hasMoreTokens()) {
             String s = words.nextToken();
             s = encode(s) + " ";
             encrypted_message.append(s);
         }
     }
-    public final void decrypt(String message)
-    {
-        /* The encoded message string is tokenized into individual words,
-         * and each word is encoded by calling the decode method
-         */
-// Supply the code that will decrypt the encrypted string
+
+    public final void decrypt(String message) {
+        decrypted_message = new StringBuffer();
+        StringTokenizer words = new StringTokenizer(message);
+        while (words.hasMoreTokens()) {
+            String s = words.nextToken();
+            s = decode(s) + " ";
+            decrypted_message.append(s);
+        }
     }
-    public String getEncodedMessage()
-    {
+
+    public String getEncodedMessage() {
         return encrypted_message.toString();
     }
-    public String getDecodedMessage()
-    {
+
+    public String getDecodedMessage() {
         return decrypted_message.toString();
     }
+
     public abstract String encode(String s);
+
     public abstract String decode(String s);
 }
-
